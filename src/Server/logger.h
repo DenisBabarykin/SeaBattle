1a0fed01e04d9cadad1a6d63f14df7f502b73371
efgefg0001 <efg.efg.0001@gmail.com>
2015-10-17 16:03:28 +0300

    Added strings for formats

--- src/Server/logger.h
+++ src/Server/logger.h
@@ -2,33 +2,35 @@
 #define LOGGER_H
 
 #include <QtGlobal>
+#include <QDate>
 #include <QCoreApplication>
 #include <QFile>
 #include <QTextStream>
 
+const QString DATETIME_FORMAT = "dd.MM.yyyy hh:mm:ss";
+const QString LOG_FILENAME = "log.txt";
+const QString LOG_STR_FORMAT = "%1, %2: %3";
+
 void logMessagesInFile(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
-    QByteArray localMsg = msg.toLocal8Bit();
-    QFile file("log.txt");
+//    QByteArray localMsg = msg.toLocal8Bit();
+    QFile file(LOG_FILENAME);
 
     if (file.open(QIODevice::Append | QIODevice::Text)) {
         QTextStream stream(&file);
+        QString datetime = QDateTime::currentDateTime().toString(DATETIME_FORMAT);
         switch (type) {
         case QtDebugMsg:
-            stream << "Debug: " << localMsg.constData() << ", " << context.file << "," << context.line << "," << context.function << endl;
+            stream << LOG_STR_FORMAT.arg(datetime, "Info", msg) << endl;
             break;
-            /*
-    case QtInfoMsg:
-        fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-        break;*/
         case QtWarningMsg:
-            stream << "Warning: " << localMsg.constData() << ", " << context.file << ", " << context.line << ", " << context.function << endl;
+            stream << LOG_STR_FORMAT.arg(datetime, "Warning", msg) << endl;
             break;
         case QtCriticalMsg:
-            stream << "Critical: " << localMsg.constData() << ", " << context.file << ", " << context.line << ", " << context.function << endl;
+            stream << LOG_STR_FORMAT.arg(datetime, "Critical", msg) << endl;
             break;
         case QtFatalMsg:
-            stream << "Fatal: " << localMsg.constData() << ", " << context.file << ", " << context.line << ", " << context.function << endl;
+            stream << LOG_STR_FORMAT.arg(datetime, "Fatal", msg) << endl;
             QCoreApplication::exit(-1);
         }
     }
